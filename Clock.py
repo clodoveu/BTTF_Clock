a4adb1a299800bb4ac173031671cfa3bfefaba80
clodoveu <clodoveu@dcc.ufmg.br>
2019-07-13 11:13:55 -0300

    Adding the temperature and humidity sensor; beep at startup; correction for shutdown procedure

--- Clock.py
+++ Clock.py
@@ -215,7 +215,7 @@ def Clock_temp_humid():
     CM = globals.ClockMode
     while CM == globals.ClockMode and globals.running.is_set():
         humidity, temperature = Adafruit_DHT.read_retry(globals.SensorType, globals.SensorPin)
-        if humidity is none or temperature is none:
+        if humidity is None or temperature is None:
             humidity = 0.0
             temperature = 0.0
         d0 = datetime.now()
